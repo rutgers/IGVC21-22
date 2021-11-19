347d73792090b51ed6e1c69e21871d26ff970cc2
Rutgers IEEE <vexu.rutgersieee@gmail.com>
2021-11-19 15:59:53 -0500

    added URDF testing stuff

---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/ieee/IGVC21-22/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/ieee/n2e_nav_lecture/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/ieee/n2e_nav_lecture/catkin_ws/devel;/opt/ros/noetic' != '' else []
---
+++ catkin_ws/build/urdf_testing/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ieee/IGVC21-22/catkin_ws/src/urdf_testing
+# Build directory: /home/ieee/IGVC21-22/catkin_ws/build/urdf_testing
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.

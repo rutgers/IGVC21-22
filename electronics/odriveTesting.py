a5f9abb674c93e637842be2518951c6db173c69c
contiello <67182906+drm-solister@users.noreply.github.com>
2022-04-01 18:01:01 -0400

    updated commands to run motor

--- electronics/odriveTesting.py
+++ electronics/odriveTesting.py
@@ -10,11 +10,24 @@ if (odrv0.axis0.motor.config.pre_calibrated != True):
 	print("not calibrated")
 	pass 
 
-
-
-
+odrv0.axis0.motor.config.current_lim = 9.4
+odrv0.axis0.motor.config.pole_pairs = 3
+odrv0.axis0.motor.config.torque_constant = 0.05
+odrv0.axis0.encoder.config.cpr = 4096
+# odrv0.axis0.motor.config.current_lim = ?
+axis.controller.config.input_filter_bandwidth = 2.0
+odrv0.axis0.controller.config.vel_gain = 0.045
+odrv0.axis0.controller.config.vel_limit = 40
 odrv0.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
-odrv0.axis0.controller.input_pos = 10
 
+# for position control
+#	odrv0.axis0.controller.config.input_mode = INPUT_MODE_POS_FILTER
+#	ordv0.axis0.controller.input_pos = 1
 
+# for velocity control
+#	odrv0.axis0.controller.config.control_mode = CONTROL_MODE_VELOCITY_CONTROL
+#	odrv0.axis0.controller.input_vel = 1
+
+# note difference between control mode and input mode. to make velocity control work, make sure
+# control mode is 0
 

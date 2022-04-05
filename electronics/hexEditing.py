5de16391a50cb7e057a37038f9f79fd3ab666354
Ronan <67182906+drm-solister@users.noreply.github.com>
2022-04-05 16:16:40 -0400

    removed old motor controller stuff

--- electronics/hexEditing.py
+++
@@ -1,8 +0,0 @@
-import mmap
-
-with open('./controllerFiles/INFO.BIN', 'r+b') as file:
-    mm = mmap.mmap(file.fileno(), 0)
-    print(mm[int('1F', 16)])
-    #print(file.readlines()) 
-
-    # if this isnt the object dictionary then what the heck is
